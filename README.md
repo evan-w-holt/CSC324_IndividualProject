de624282bae63c0cc0087f769df1aa6f600eaa56
evan-w-holt <holtevan@grinnell.edu>
2019-09-15 16:28:45 -0500

    HW3 Make home, about, and help mostly-static pages

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Individual project for CSC-324
+### Individual project for CSC-324
+
+### Edigaul Abugida Text Tool
+A tool for transliterating from the English alphabet into the Edigaul abugida (a fictional writing system). I made a series of images to use as an Edigaul font last spring for a piece of software I intended to make that did this transliterating, and this seemed like a good opportunity to actually get around to making that software.
+
+See the about page of the site for info on what Edigaul is and what an abugida is.
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | Edigaul Abugida"
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | Edigaul Abugida"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | Edigaul Abugida"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About Edigaul | Edigaul Abugida"
+  end
+
+end
