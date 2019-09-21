ed4bcde02aa0c6d4ab414fe62f0c3d9260dac838
evan-w-holt <holtevan@grinnell.edu>
2019-09-20 19:28:16 -0500

    Add Edigaul images to random word generator

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,36 +2,42 @@ class StaticPagesController < ApplicationController
 
   # At some point I'll figure out a better way to store these
   # so I can have a lot of words at my disposal
-
-  # TODO: Make these into images
   ALL_ROHKSHE = {
-    :water => ["[ithni]", "ithni"],
-    :river => ["[kuthni]", "kuthni"],
-    :ocean => ["[muuthni]", "muuthni"],
-    :'to have' => ["[helai]", "helai"],
-    :'to be' => ["[ekai]", "ekai"],
-    :hello => ["[oyseh]", "oyseh"],
-    :goodbye => ["[vastin]", "vastin"],
-    :I => ["[zai]", "zai"],
-    :you => ["[zo]", "zo"],
-    :one => ["[sikla]", "sikla"],
-    :two => ["[pulta]", "pulta"],
-    :three => ["[treena]", "treena"]
+    :Edigaul => ["Edigaul", [["eh", "D", nil], ["ih", "G", nil], ["aw", "L", nil]]],
+    :Rohkshe => ["Rohkshe", [[nil, "R", nil], ["aw", "K", nil], [nil, "Sh", "ee"]]],
+    :water => ["ithni", [["ih", "Th", nil], [nil, "N", "ee"]]],
+    :river => ["kuthni", [[nil, "K", "uh"], [nil, "Th", nil], [nil, "N", "ee"]]],
+    :ocean => ["muuthni", [[nil, "M", "uuh"], [nil, "Th", nil], [nil, "N", "ee"]]],
+    :'to have' => ["helai", [[nil, "H", nil], ["eh", "L", "ie"]]],
+    :'to be' => ["ekai", [["eh", "K", "ie"]]],
+    :hello => ["oyseh", [["oh", "Y", nil], [nil, "S", "eh"]]],
+    :goodbye => ["vastin", [[nil, "V", nil], ["ah", "S", nil], [nil, "T", nil], ["ih", "N", nil]]],
+    :I => ["zai", [[nil, "Z", "ie"]]],
+    :you => ["zo", [[nil, "Z", "oh"]]],
+    :one => ["sikla", [[nil, "S", nil], ["ih", "K", nil], [nil, "L", "uh"]]],
+    :two => ["pulta", [[nil, "P", nil], ["oo", "L", nil], [nil, "T", "uh"]]],
+    :three => ["treena", [[nil, "T", nil], [nil, "R", "ee"], [nil, "N", "uh"]]],
+    :this => ["kish", [[nil, "K", nil], ["ih", "Sh", nil]]],
+    :that => ["kahsh", [[nil, "K", nil], ["ah", "Sh", nil]]],
+    :and => ["oh", [["oh", "SP", nil]]]
   }
   ALL_WORDS = [
+    :Edigaul, :Rohkshe,
     :water, :river, :ocean,
     :'to have', :'to be',
     :hello, :goodbye,
     :I, :you,
-    :one, :two, :three
+    :one, :two, :three,
+    :this, :that,
+    :and
   ]
 
   def home
     @translation = ALL_WORDS.sample
 
     random_word = ALL_ROHKSHE[@translation]
-    @rohkshe = random_word[0]
-    @transliteration = random_word[1]
+    @transliteration = random_word[0]
+    @rohkshe = random_word[1]
     @translation = @translation.to_s
   end
 
