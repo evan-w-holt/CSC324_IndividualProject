97fb5641e48e58647339bd72247a33ad6c94f2a8
evan-w-holt <holtevan@grinnell.edu>
2019-09-28 11:33:58 -0500

    Add tests to ensure all edigaul text boxes, words, and units have proper contents

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -54,15 +54,47 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@site_title}"
   end
 
-  # Home page random word tests ==================================================
+  # Home page random word table tests ==================================================
 
-  test "random word table should not be empty" do
+  test "random word table should exist with headers" do
     get @home_url
 
     assert_select "td", 6
-    assert_select "td", {:text=>"Rohkshe", :count=>1}
-    assert_select "td", {:text=>"English Transliteration", :count=>1}
-    assert_select "td", {:text=>"English Translation", :count=>1}
+    assert_select "td", {:text => "Rohkshe", :count => 1}
+    assert_select "td", {:text => "English Transliteration", :count => 1}
+    assert_select "td", {:text => "English Translation", :count => 1}
+  end
+
+  # Home page edigaul tests ==================================================
+
+  test "each edigaul text box should contain at least one edigaul word" do
+    get @home_url
+
+    assert_select "span.edigaulTextBox" do |elements|
+      elements.each do |element|
+        assert_select element, "span.edigaulWord", :minimum => 1
+      end
+    end
+  end
+
+  test "each edigaul word should contain at least one edigaul unit" do
+    get @home_url
+
+    assert_select "span.edigaulWord" do |elements|
+      elements.each do |element|
+        assert_select element, "span.edigaulUnit", :minimum => 1
+      end
+    end
+  end
+
+  test "each edigaul unit should contain between one and three img" do
+    get @home_url
+
+    assert_select "span.edigaulUnit" do |elements|
+      elements.each do |element|
+        assert_select element, "img", {:minimum => 1, :maximum => 3}
+      end
+    end
   end
 
 end
