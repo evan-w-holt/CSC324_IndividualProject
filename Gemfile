e9efe1759790ce214704f7c85c46a733a59cec5d
evan-w-holt <holtevan@grinnell.edu>
2019-09-28 00:18:34 -0500

    Split and debug old tests, add one test for homework 4 additions

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
+  gem 'rails-controller-testing'
 end
 
 group :development do
@@ -51,10 +52,9 @@ group :development do
 end
 
 group :test do
-  gem 'rails-controller-testing', '0.1.1'
-  gem 'minitest-reporters',       '1.1.9'
-  gem 'guard',                    '2.13.0'
-  gem 'guard-minitest',           '2.4.4'
+  gem 'minitest-reporters', '1.1.9'
+  gem 'guard',              '2.13.0'
+  gem 'guard-minitest',     '2.4.4'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,28 +2,67 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @site_title = "Edigaul Abugida"
+
+    @home_url = "/"
+    @help_url = "/help"
+    @about_url = "/about"
+  end
+
+  # Page getting tests ==================================================
+
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | Edigaul Abugida"
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get @home_url
     assert_response :success
-    assert_select "title", "Home | Edigaul Abugida"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get @help_url
     assert_response :success
-    assert_select "title", "Help | Edigaul Abugida"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get @about_url
     assert_response :success
-    assert_select "title", "About Edigaul | Edigaul Abugida"
+  end
+
+  # Page title tests ==================================================
+
+  test "root page should have home in title" do
+    get root_url
+    assert_select "title", "Home | #{@site_title}"
+  end
+
+  test "home page should have home in title" do
+    get @home_url
+    assert_select "title", "Home | #{@site_title}"
+  end
+
+  test "help page should have help in title" do
+    get @help_url
+    assert_select "title", "Help | #{@site_title}"
+  end
+
+  test "about page should have about in title" do
+    get @about_url
+    assert_select "title", "About | #{@site_title}"
+  end
+
+  # Home page random word tests ==================================================
+
+  test "random word table should not be empty" do
+    get @home_url
+
+    assert_select "td", 6
+    assert_select "td", {:text=>"Rohkshe", :count=>1}
+    assert_select "td", {:text=>"English Transliteration", :count=>1}
+    assert_select "td", {:text=>"English Translation", :count=>1}
   end
 
 end
