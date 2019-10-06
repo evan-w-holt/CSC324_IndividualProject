bb6e7c9f0ad7f1e875e446cb27d5640d9474bf63
evan-w-holt <holtevan@grinnell.edu>
2019-10-05 21:22:06 -0500

    Add tests for word model

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,5 @@
 class Word < ApplicationRecord
-  ROHKSHE_FORMAT_REGEX = /(\[\w{0,3}\+\w{1,2}\+\w{0,3}\])+/i
+  ROHKSHE_FORMAT_REGEX = /\A(\[\w{0,3}\+\w{1,2}\+\w{0,3}\])+\z/i
 
   validates :rohkshe, presence: true, format: { with: ROHKSHE_FORMAT_REGEX }
   validates :transliteration, presence: true
--- test/models/word_test.rb
+++ test/models/word_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class WordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @valid_word = Word.new(
+      rohkshe: "[+k+uh][+th+][+n+ee]",
+      transliteration: "kuthni",
+      translation: "river"
+    )
+
+    @no_rohkshe = Word.new(
+      rohkshe: "     ",
+      transliteration: "kuthni",
+      translation: "river"
+    )
+
+    @no_transliteration = Word.new(
+      rohkshe: "[+k+uh][+th+][+n+ee]",
+      transliteration: "     ",
+      translation: "river"
+    )
+
+    @no_translation = Word.new(
+      rohkshe: "[+k+uh][+th+][+n+ee]",
+      transliteration: "kuthni",
+      translation: "     "
+    )
+
+    @invalid_rohkshe = Word.new(
+      rohkshe: "[+k+uh]in[+th+]val[+n+ee]id",
+      transliteration: "kuthni",
+      translation: "river"
+    )
+  end
+
+  # Validation tests ==================================================
+
+  test "valid word should be valid" do
+    assert @valid_word.valid?
+  end
+
+  test "missing rohkshe should be invalid" do
+    assert_not @no_rohkshe.valid?
+  end
+
+  test "missing transliteration should be invalid" do
+    assert_not @no_transliteration.valid?
+  end
+
+  test "missing translation should be invalid" do
+    assert_not @no_translation.valid?
+  end
+
+  test "rohkshe that does not follow regex should be invalid" do
+    assert_not @invalid_rohkshe.valid?
+  end
 end
