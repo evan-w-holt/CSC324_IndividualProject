dca7a9201c94e15064b0efba317e0090b19c98bd
evan-w-holt <holtevan@grinnell.edu>
2019-10-05 21:04:10 -0500

    Add validation checks in word model

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,7 @@
 class Word < ApplicationRecord
+  ROHKSHE_FORMAT_REGEX = /(\[\w{0,3}\+\w{1,2}\+\w{0,3}\])+/i
+
+  validates :rohkshe, presence: true, format: { with: ROHKSHE_FORMAT_REGEX }
+  validates :transliteration, presence: true
+  validates :translation, presence: true
 end
