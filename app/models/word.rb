603ec9b26d020782c09e62458278c3db1a7dba58
evan-w-holt <holtevan@grinnell.edu>
2019-11-12 19:47:11 -0600

    Make regex validation fail messages more specific in word model

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,12 +2,13 @@ class Word < ApplicationRecord
   before_save { self.rohkshe = rohkshe.downcase }
 
   ROHKSHE_FORMAT_REGEX = /\A(\[\w{0,3}\+\w{1,2}\+\w{0,3}\])+\z/i
-  VALID_LETTERS_REGEX = /\A(\[(eh|ee|ah|aah|aw|ay|uh|uuh|oh|oo|ih|ie)?\+(b|d|f|g|h|j|k|l|m|n|p|r|s|t|v|w|y|z|sh|ch|th|zh|x})\+(eh|ee|ah|aah|aw|ay|uh|uuh|oh|oo|ih|ie)?\])+\z/i
+  VALID_LETTERS_REGEX = /\A(\W*(eh|ee|ah|aah|aw|ay|uh|uuh|oh|oo|ih|ie)?\W+(b|d|f|g|h|j|k|l|m|n|p|r|s|t|v|w|y|z|sh|ch|th|zh|x})\W+(eh|ee|ah|aah|aw|ay|uh|uuh|oh|oo|ih|ie)?\W*)+\z/i
 
   validates :rohkshe, presence: true, format: {
     with: ROHKSHE_FORMAT_REGEX,
     message: "invalid, each letter should look like: [<vowel/empty>+<consonant>+<vowel/empty>]"
-  }, format: {
+  }
+  validates :rohkshe, format: {
     with: VALID_LETTERS_REGEX,
     message: "invalid, some vowels or consonants are not valid"
   }
