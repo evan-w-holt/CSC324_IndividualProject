df9c6802f8c455bfecae0b2239f533a3b716ae0b
evan-w-holt <holtevan@grinnell.edu>
2019-11-12 19:25:03 -0600

    Fix regex validation for word model

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,7 +1,7 @@
 class Word < ApplicationRecord
   before_save { self.rohkshe = rohkshe.downcase }
 
-  ROHKSHE_FORMAT_REGEX = /\A(\[\w{0,3}\+\w{1,2}\+\w{0,3}\])+\z/i
+  ROHKSHE_FORMAT_REGEX = /\A(\[(eh|ee|ah|aah|aw|ay|uh|uuh|oh|oo|ih|ie)?\+(b|d|f|g|h|j|k|l|m|n|p|r|s|t|v|w|y|z|sh|ch|th|zh|x})\+(eh|ee|ah|aah|aw|ay|uh|uuh|oh|oo|ih|ie)?\])+\z/i
 
   validates :rohkshe, presence: true, format: {
     with: ROHKSHE_FORMAT_REGEX,
